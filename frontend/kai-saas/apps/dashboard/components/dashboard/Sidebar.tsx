c98d68bb01b4bf545da2a5269f2da4edfc77da7f
arturoardura <arturo@talktokai.com>
2025-12-03 13:47:43 -0600

    feat: add chevron icons to Sidebar agent list
    - Added upward chevron icon to Super Sanchez (expanded state)
    - Added downward chevron icons to all other agents (collapsed state)
    - Added chevron to QR section
    - Super Sanchez uses white chevron, other agents use dark chevrons
    - QR section uses semi-transparent white chevron
    - Replaced placeholder elements with proper SVG icons

--- frontend/kai-saas/apps/dashboard/components/dashboard/Sidebar.tsx
+++ frontend/kai-saas/apps/dashboard/components/dashboard/Sidebar.tsx
@@ -9,7 +9,12 @@ export default function Sidebar() {
       <div className="w-36 h-16 left-[37px] top-[45px] absolute opacity-90 justify-start text-neutral-900/80 text-base font-light font-['Nunito_Sans'] leading-8">
         Editar Agente<br/>Tools<br/>Enviar recordatorios
       </div>
-      <div className="w-1.5 h-1 left-[189px] top-[27px] absolute origin-top-left rotate-180 outline outline-2 outline-offset-[-1px] outline-white" />
+      {/* Super Sanchez Arrow - Up */}
+      <div className="absolute left-[186px] top-[23px]">
+        <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 5L4.5 1L8 5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
       <div className="w-[3px] h-20 left-[24px] top-[51px] absolute bg-blue-900/20 rounded-[10px]" />
       
       {/* Agent List */}
@@ -20,13 +25,37 @@ export default function Sidebar() {
       <div className="w-9 left-[21px] top-[312px] absolute justify-start text-black/80 text-base font-bold font-['Nunito_Sans']">ADO</div>
       <div className="w-28 left-[21px] top-[354px] absolute justify-start text-black/80 text-base font-bold font-['Nunito_Sans']">Feria Tabasco</div>
       
-      {/* Arrows */}
-      <div className="w-2 h-1 left-[181.54px] top-[152px] absolute bg-black/80 outline outline-2 outline-offset-[-1px] outline-neutral-900" />
-      <div className="w-2 h-1 left-[181.54px] top-[194px] absolute bg-black/80 outline outline-2 outline-offset-[-1px] outline-neutral-900" />
-      <div className="w-2 h-1 left-[181.54px] top-[236px] absolute bg-black/80 outline outline-2 outline-offset-[-1px] outline-neutral-900" />
-      <div className="w-2 h-1 left-[181.54px] top-[278px] absolute bg-black/80 outline outline-2 outline-offset-[-1px] outline-neutral-900" />
-      <div className="w-2 h-1 left-[181.54px] top-[320px] absolute bg-black/80 outline outline-2 outline-offset-[-1px] outline-neutral-900" />
-      <div className="w-2 h-1 left-[181.54px] top-[362px] absolute bg-black/80 outline outline-2 outline-offset-[-1px] outline-neutral-900" />
+      {/* Agent Arrows - Down (rotated 180deg) */}
+      <div className="absolute left-[178px] top-[148px] rotate-180">
+        <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 5L4.5 1L8 5" stroke="#161717" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
+      <div className="absolute left-[178px] top-[190px] rotate-180">
+        <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 5L4.5 1L8 5" stroke="#161717" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
+      <div className="absolute left-[178px] top-[232px] rotate-180">
+        <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 5L4.5 1L8 5" stroke="#161717" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
+      <div className="absolute left-[178px] top-[274px] rotate-180">
+        <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 5L4.5 1L8 5" stroke="#161717" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
+      <div className="absolute left-[178px] top-[316px] rotate-180">
+        <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 5L4.5 1L8 5" stroke="#161717" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
+      <div className="absolute left-[178px] top-[358px] rotate-180">
+        <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 5L4.5 1L8 5" stroke="#161717" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
       
       {/* QR Section */}
       <div className="w-48 h-60 left-[10px] top-[535px] absolute bg-gradient-to-b from-amber-200/30 to-white/30 rounded-[10px] shadow-[0px_0px_20px_0px_rgba(1,0,245,0.10)] shadow-[inset_0px_0px_26px_21px_rgba(255,255,255,0.50)]" />
@@ -34,7 +63,11 @@ export default function Sidebar() {
       <div className="w-32 h-12 left-[41px] top-[708px] absolute text-center justify-start text-neutral-800 text-base font-normal font-['Nunito_Sans'] leading-4">
         Escanea y genera tu Agente desde WhatsApp
       </div>
-      <div className="w-1.5 h-1 left-[178px] top-[549px] absolute mix-blend-difference outline outline-2 outline-offset-[-1px] outline-white/50" />
+      <div className="w-1.5 h-1 left-[178px] top-[549px] absolute">
+      <svg width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 5L4.5 1L8 5" stroke="#161717" strokeOpacity="0.8" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+        </div>
     </div>
   );
 }
