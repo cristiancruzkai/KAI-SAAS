b0ff2d4afb32adb2e4bbdd73337a94f4b5960dd1
arturoardura <arturo@talktokai.com>
2025-12-04 19:00:47 -0600

    cleaned front

--- app/lib/firebase.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp, getApps } from "firebase/app";
-
-const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-};
-
-export const app =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
